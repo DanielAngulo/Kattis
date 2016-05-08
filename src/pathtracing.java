f964138c8c00d1e9708390dd2d43b1e935110776
DanielAngulo <dangulosanchez@yahoo.com>
2016-05-08 01:43:25 -0400

    Update pathtracing.java
    Completely redone, a lot shorter and faster.

--- src/pathtracing.java
+++ src/pathtracing.java
@@ -1,161 +1,112 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 public class pathtracing {
-	static ArrayList<String> directions = new ArrayList<String>();
-	static String[] instructions;
-	static Scanner scan = new Scanner(System.in);
-	static ArrayList<String> map = new ArrayList<String>();
+
 	public static void main(String[] args) {
-		while(scan.hasNext())
-			directions.add(scan.next());
-		instructions = directions.toArray(new String[directions.size()]);
-		int h = 0;
-		int w = 0;
-		int width = getWidth() + 1;
-		setMap(width, getWidth() + 1);
-		map.set(0, "S" + setSpace(width - 1));
-		for(String follow: instructions){
-			if(follow.equals("up")){
-				h--;
-				if(h < 0){
-					map.add(0, map.get(1).replaceAll("[^ ]", " "));
-					h++;
-				}
-			}
-			else if(follow.equals("down")){
-				h++;
-				if(h > map.size()){
-					map.add(setSpace(width));
-				}
-			}
-			else if(follow.equals("left")){
-				w--;
-				if(w < 0){
-					fixLeft();
-					w++;
-				}
-			}
-			else if(follow.equals("right")){
-				w++;
-				if(w > map.get(h).length()){
-					fixRight();
-				}
-			}
-			if(h >= map.size())
-				map.add(map.get(0).replaceAll("[^ ]", " "));
-			if(w >= map.get(h).length())
-				fixRight();
-			String str = map.get(h);
-			str = set(str, w);
-			map.set(h, str);
+		Scanner scan = new Scanner(System.in);
+		ArrayList<String> directions = new ArrayList<String>();
+		while(scan.hasNext()){
+			String str = scan.next();
+			if(str.equals("stop"))
+				break;
+			else
+				directions.add(str);
 		}
-		if(h >= map.size())
-			h = map.size() - 1;
-		if(w >= map.get(h).length())
-			w = map.get(h).length() - 1;
-		String str = map.get(h);
-		char[] temp = str.toCharArray();
-		temp[w] = 'E';
-		map.set(h, new String(temp));
-		printFinal();
-	}
-	
-	private static int getRandom(int min, int max) {
-		return (int)(Math.random() * (max - min) + min);
+		ArrayList<String> board = new ArrayList<String>();
+		for(int i = 0; i < 10; i++)
+			board.add(" ");
+		board.set(0, "S");
+		int currentH = 0;
+		int currentW = 0;
+		for(String str : directions){
+			if(str.equals("up")){
+				currentH--;
+				if(currentH == -1){
+					board.add(0, newString(board.get(1).length()));
+					currentH = 0;
+				}
+			}
+			else if(str.equals("down")){
+				currentH++;
+				if(currentH == board.size())
+					board.add(newString(board.get(0).length()));
+			}
+			else if(str.equals("left")){
+				currentW--;
+				if(currentW == -1){
+					currentW = 0;
+					addSpace(board, true);
+				}
+			}
+			else{
+				currentW++;
+				if(currentW >= board.get(0).length())
+					addSpace(board, false);
+			}
+			board.set(currentH, setStar(board.get(currentH).toCharArray(), currentW));
+		}
+		if(currentH >= board.size())
+			currentH = board.size() - 1;
+		if(currentW >= board.get(currentH).length())
+			currentW = board.get(currentH).length() - 1;
+		char[] temp = board.get(currentH).toCharArray();
+		temp[currentW] = 'E';
+		board.set(currentH, new String(temp));
+		trimBoard(board);
+		printBoard(board);
 	}
 
-	public static String set(String str, int w){
-		char[] array = str.toCharArray();
-		if(array[w] != 'S')
-			array[w] = '*';
-		return new String(array);
-	}
-	
-	public static void fixLeft(){
-		for(int i = 0; i < map.size(); i++){
-			map.set(i, " " + map.get(i));
-		}
-	}
-	
-	public static void fixRight(){
-		for(int i = 0; i < map.size(); i++){
-			map.set(i, map.get(i) + " ");
-		}
-	}
-	
-	public static int getHeight(){
-		int[] absolute = new int[directions.size() - 1];
-		if(directions.get(0).equals("up"))
-			absolute[0] = 1;
-		else if(directions.get(1).equals("down"))
-			absolute[0] = -1;
-		for(int i = 1; i < absolute.length; i++){
-			String str = directions.get(i);
-			if(str.equals("up")){
-				absolute[i] = absolute[i - 1] + 1;
-			}
-			else if(str.equals("down"))
-				absolute[i] = absolute[i - 1] - 1;
-		}
-		Arrays.sort(absolute);
-		return Math.abs(absolute[0]) + absolute[absolute.length - 1];
-	}
-	
-	public static int getWidth(){
-		int count = 0;
-		for(String str : directions)
-			if(str.equals("left") || str.equals("right"))
-				count++;
-		return count;
-	}
-	
-	public static void setMap(int h, int w){
+	private static String newString(int length) {
 		String str = "";
-		for(int i = 0; i <= w; i++)
-			str += " ";
-		for(int i = 0; i < h; i++)
-			map.add(str);
-	}
-	
-	public static String setSpace(int w){
-		String str = "";
-		for(int i = 0; i <= w; i++)
+		for(int i = 0; i < length; i++)
 			str += " ";
 		return str;
 	}
-	
-	public static void printFinal(){
-		eraseBlank();
-		String border = "";
-		for(int i = 0; i < map.get(0).length() + 2; i++)
-			border += "#";
-		System.out.println(border);
-		for(String str : map)
-			System.out.println("#" + str + "#");
-		System.out.println(border);
+
+	private static void addSpace(ArrayList<String> board, boolean start) {
+		int size = board.size();
+		if(start)
+			for(int i = 0; i < size; i++)
+				board.set(i, " " + board.get(i));
+		else
+			for(int i = 0; i < size; i++)
+				board.set(i, board.get(i) + " ");
+	}
+
+	private static String setStar(char[] string, int currentW) {
+		if(string[currentW] != 'S')
+			string[currentW] = '*';
+		return new String(string);
 	}
 	
-	public static void eraseBlank(){
-		for(int i = 0; i < map.size(); i++){
-			if(map.get(i).trim().equals("")){
-				map.remove(i);
+	private static void trimBoard(ArrayList<String> board) {
+		for(int i = 0; i < board.size(); i++){
+			if(board.get(i).trim().equals("")){
+				board.remove(i);
 				i--;
 			}
 		}
-		for(int i = 0; i < map.get(0).length(); i++){
+		int length = board.get(0).length();
+		for(int i = 0; i < length; i++){
 			String str = "";
-			for(String x : map){
+			for(String x : board)
 				str += x.charAt(i);
-			}
-			if(str.trim().equals("")){
-				for(int x = 0; x < map.size(); x++){
-					String current = map.get(x);
-					current = current.substring(0, i) + current.substring(i + 1);
-					map.set(x, current);
-				}
-				i--;
+			if(str.trim().length() == 0){
+				for(int x = 0; x < board.size(); x++)
+					board.set(x, board.get(x).substring(0, i));
+				break;
 			}
 		}
+	}
+	
+	private static void printBoard(ArrayList<String> board) {
+		int length = board.get(0).length() + 2;
+		String border = "";
+		for(int i = 0; i < length; i++)
+			border += "#";
+		System.out.println(border);
+		for(String str : board)
+			System.out.println("#" + str + "#");
+		System.out.println(border);
 	}
 }
