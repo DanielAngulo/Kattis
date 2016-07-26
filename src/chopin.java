b6601078f37f18ab20ddb6e88efbd34a8bd94543
Daniel Angulo <dangulosanchez@yahoo.com>
2016-07-26 12:39:36 -0400

    Update chopin.java

--- src/chopin.java
+++ src/chopin.java
@@ -1,36 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-
-public class chopin {
-
-	public static void main(String[] args) {
-		String[] notes = {"A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"};
-		Scanner scan = new Scanner(System.in);
-		int current = 1;
-		while(scan.hasNextLine()){
-			String[] array = scan.nextLine().split(" ");
-			boolean keep = true;
-			if(array[0].length() == 1){
-				System.out.println("Case " + current +": UNIQUE");
-			}
-			else{
-				while(keep){
-					String searching = array[0];
-					for(String str : notes){
-						String[] temp = str.split("/");
-						if(temp.length > 1){
-							if(temp[0].equals(searching)){
-								System.out.println("Case " + current + ": " + temp[1] + " " + array[1]);
-								keep = false;
-							}
-							else if(temp[1].equals(searching)){
-								System.out.println("Case " + current + ": " + temp[0] + " " + array[1]);
-								keep = false;
-							}
-						}
-					}
-				}
-			}
-			current++;
-		}
-	}
+public class chopin
+{
+    public static void main(String[] args)
+    {
+        HashMap<String, String> notes = new HashMap<>();
+        notes.put("A", "");
+        notes.put("A#", "Bb");
+        notes.put("B", "");
+        notes.put("C", "");
+        notes.put("C#", "Db");
+        notes.put("D", "");
+        notes.put("D#", "Eb");
+        notes.put("E", "");
+        notes.put("F", "");
+        notes.put("F#", "Gb");
+        notes.put("G", "");
+        notes.put("G#", "Ab");
+        Scanner scan = new Scanner(System.in);
+        int cases = 1;
+        while(scan.hasNext())
+        {
+            String[] line = scan.nextLine().split(" ");
+            if(notes.containsKey(line[0]))
+            {
+                String second = notes.get(line[0]);
+                if(second.equals(""))
+                    System.out.println("Case " + cases + ": UNIQUE");
+                else
+                    System.out.println("Case " + cases + ": " + second + " " + line[1]);
+            }
+            else
+            {
+                boolean found = false;
+                while(!found)
+                {
+                    for(Map.Entry<String, String> entry : notes.entrySet())
+                    {
+                        if(entry.getValue().equals(line[0]))
+                        {
+                            System.out.println("Case " + cases + ": " + entry.getKey() + " " + line[1]);
+                            found = true;
+                        }
+                    }
+                }
+            }
+            cases++;
+        }
+        scan.close();
+    }
 }
